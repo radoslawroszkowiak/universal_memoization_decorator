95d24e8314e2c9a9e8baef8d4fadc238f925871d
Radosław Roszkowiak <rroszkowiak@gmail.com>
2015-01-23 20:33:06 +0100

    memoization decorator file added

---
+++ memoized.py
@@ -0,0 +1,19 @@
+import functools
+import pickle
+
+
+def memoize_all(func):
+    cache = {}
+
+    @functools.wraps(func)
+    def cached(*args, **kwargs):
+        arg_name = func.func_code.co_varnames
+        arg_dict = {}
+        for i, arg in enumerate(args):
+            arg_dict[arg_name[i]] = args[i]
+        arg_dict.update(**kwargs)
+        key = pickle.dumps(arg_dict)
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return cached
